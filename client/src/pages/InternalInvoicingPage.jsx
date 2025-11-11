f1c5620aaeda384845ad3263446f97b4e0e0ac43
rakhitha <rakhithaprabashwara@gmail.com>
2025-11-11 09:57:46 +0000

    invoicing update

--- client/src/pages/InternalInvoicingPage.jsx
+++ client/src/pages/InternalInvoicingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { FaSpinner, FaPencilAlt, FaDownload, FaExclamationTriangle } from 'react-icons/fa';
 import {
     getInternalInvoicingReport, createInternalInvoice, updateRecordAccountingMonth,
